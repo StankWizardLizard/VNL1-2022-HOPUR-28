1e1fd9051685f36b61d4b0a2cedaf1605b56f2a7
Chris Lilliendahl <christiank20@ru.is>
2022-11-28 15:45:13 +0000

    Methods for logic wrapper

--- RU_Basement_Open/logic/logic_wrapper.py
+++ RU_Basement_Open/logic/logic_wrapper.py
@@ -1,5 +1,19 @@
 from data.data_wrapper import Data_Wrapper
+from logic.player_LL import PlayerLL
 
-class Logic_Wrapper:
+class LogicWrapper:
     def __init__(self):
         self.data_wrapper = Data_Wrapper()
+        self.player_logic = PlayerLL(self.data_wrapper)
+        
+    def create_player(self, player):
+        # TODO: remove variable once IO connected 
+        a = self.player_logic.create_player(player) 
+        return a
+    
+    def get_player(self, id):
+        pass
+    
+    def get_all_players(self):
+        pass
+    
